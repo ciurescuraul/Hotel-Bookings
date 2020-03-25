a5ef614ebfb1b858f48f7598abf1dc263380d5b0
Ciurescu Raul <ciurescuraul@gmail.com>
2020-03-25 08:59:34 -1000

    REST Controllers

---
+++ src/main/java/ro/cyberit/springbootdemo/DatabaseSeeder.java
@@ -0,0 +1,11 @@
+package ro.cyberit.springbootdemo;
+
+import org.springframework.boot.CommandLineRunner;
+
+public class DatabaseSeeder implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) throws Exception {
+
+    }
+}
